0c02ef7eadb7587d9f09fe6d202d3c05ab4cc840
kishan <joeko@hotmail.com>
2024-03-18 15:29:28 +0100

    Ik heb mobiel css toegevoegd en grid toegevoegd aan bijna alles moet nog alleen tablet en proberen grid aan te passen aan mobiel en tablet

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
         <link rel="stylesheet" type="text/css" href="Css/styl.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">  
+        <link href="https://fonts.googrestaurantleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap">  
         <script src="lib/Slideshow.js" defer></script>
         <script src="lib/hamburger.js" defer></script>
     </head>
     <body>
     <?php include 'header.php'; ?>
-    <main>
+        <main>
     <section class="content">
         <article class="slide">
             <img src="Images/chickenmasala.jpg" class="slideshow">
