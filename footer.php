bdf0b079356e2ee56264e98fc3b7648d4712b30a
kishan <joeko@hotmail.com>
2024-04-03 11:02:42 +0200

    heb een link toegevoegd voor reviewpage

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer>
-    <section>
-        <article class="avglink_containter">
+    <section class="link_container">
+        <article class="avg_link2">
+    <a class="avg_link" href="reciewpage.php">Review maken</a>
     <a class="avg_link" href="avg.php">Privacybeleid</a>
     </article>
     </section>
