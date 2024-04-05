85759d02ebdf77fad71431f91f02632e3b982fcd
kishan <joeko@hotmail.com>
2024-04-05 20:12:42 +0200

    heb review links in footer gezet onder privacybeleid

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <section class="link_container">
         <article class="avg_link2">
-    <a class="avg_link" href="reciewpage.php">Review maken</a>
+    <a class="avg_link" href="reviewpage.php">Review maken</a>
     <a class="avg_link" href="avg.php">Privacybeleid</a>
     </article>
     </section>
